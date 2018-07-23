1340217654a57e17d0f53dbd473f8b3a64d984b6
Oscar Leung <oscarleung1@gmail.com>
2018-07-23 15:49:15 -0700

    Finish Parser

--- proj_master/.antlr/FeatherweightJavaScriptParser.java
+++ proj_master/.antlr/FeatherweightJavaScriptParser.java
@@ -17,9 +17,10 @@ public class FeatherweightJavaScriptParser extends Parser {
 	protected static final PredictionContextCache _sharedContextCache =
 		new PredictionContextCache();
 	public static final int
-		T__0=1, T__1=2, T__2=3, T__3=4, T__4=5, IF=6, ELSE=7, WHILE=8, FUNCTION=9, 
-		VAR=10, PRINT=11, INT=12, BOOL=13, NULL=14, MUL=15, DIV=16, SEPARATOR=17, 
-		NEWLINE=18, LINE_COMMENT=19, WS=20;
+		T__0=1, T__1=2, T__2=3, T__3=4, T__4=5, T__5=6, T__6=7, IF=8, ELSE=9, 
+		WHILE=10, FUNCTION=11, VAR=12, PRINT=13, INT=14, BOOL=15, NULL=16, MUL=17, 
+		DIV=18, PLUS=19, MIN=20, MOD=21, SEPARATOR=22, GT=23, LT=24, GE=25, LE=26, 
+		EQ=27, IDENTIFIER=28, NEWLINE=29, LINE_COMMENT=30, BLOCK_COMMENT=31, WS=32;
 	public static final int
 		RULE_prog = 0, RULE_stat = 1, RULE_expr = 2, RULE_block = 3;
 	public static final String[] ruleNames = {
@@ -27,14 +28,15 @@ public class FeatherweightJavaScriptParser extends Parser {
 	};
 
 	private static final String[] _LITERAL_NAMES = {
-		null, "'('", "')'", "'%'", "'{'", "'}'", "'if'", "'else'", "'while'", 
-		"'function'", "'var'", "'print'", null, null, "'null'", "'*'", "'/'", 
-		"';'"
+		null, "'('", "')'", "'{'", "'}'", "'f'", "','", "'='", "'if'", "'else'", 
+		"'while'", "'function'", "'var'", "'print'", null, null, "'null'", "'*'", 
+		"'/'", "'+'", "'-'", "'%'", "';'", "'>'", "'<'", "'>='", "'<='", "'=='"
 	};
 	private static final String[] _SYMBOLIC_NAMES = {
-		null, null, null, null, null, null, "IF", "ELSE", "WHILE", "FUNCTION", 
-		"VAR", "PRINT", "INT", "BOOL", "NULL", "MUL", "DIV", "SEPARATOR", "NEWLINE", 
-		"LINE_COMMENT", "WS"
+		null, null, null, null, null, null, null, null, "IF", "ELSE", "WHILE", 
+		"FUNCTION", "VAR", "PRINT", "INT", "BOOL", "NULL", "MUL", "DIV", "PLUS", 
+		"MIN", "MOD", "SEPARATOR", "GT", "LT", "GE", "LE", "EQ", "IDENTIFIER", 
+		"NEWLINE", "LINE_COMMENT", "BLOCK_COMMENT", "WS"
 	};
 	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
 
@@ -118,7 +120,7 @@ public class FeatherweightJavaScriptParser extends Parser {
 				setState(11); 
 				_errHandler.sync(this);
 				_la = _input.LA(1);
-			} while ( (((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << T__0) | (1L << IF) | (1L << INT))) != 0) );
+			} while ( (((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << T__0) | (1L << T__4) | (1L << IF) | (1L << WHILE) | (1L << FUNCTION) | (1L << VAR) | (1L << PRINT) | (1L << INT))) != 0) );
 			}
 		}
 		catch (RecognitionException re) {
@@ -142,6 +144,16 @@ public class FeatherweightJavaScriptParser extends Parser {
 		public void copyFrom(StatContext ctx) {
 			super.copyFrom(ctx);
 		}
+	}
+	public static class PrintContext extends StatContext {
+		public TerminalNode PRINT() { return getToken(FeatherweightJavaScriptParser.PRINT, 0); }
+		public ExprContext expr() {
+			return getRuleContext(ExprContext.class,0);
+		}
+		public BlockContext block() {
+			return getRuleContext(BlockContext.class,0);
+		}
+		public PrintContext(StatContext ctx) { copyFrom(ctx); }
 	}
 	public static class IfThenElseContext extends StatContext {
 		public TerminalNode IF() { return getToken(FeatherweightJavaScriptParser.IF, 0); }
@@ -167,6 +179,16 @@ public class FeatherweightJavaScriptParser extends Parser {
 		}
 		public IfThenContext(StatContext ctx) { copyFrom(ctx); }
 	}
+	public static class WhileContext extends StatContext {
+		public TerminalNode WHILE() { return getToken(FeatherweightJavaScriptParser.WHILE, 0); }
+		public ExprContext expr() {
+			return getRuleContext(ExprContext.class,0);
+		}
+		public BlockContext block() {
+			return getRuleContext(BlockContext.class,0);
+		}
+		public WhileContext(StatContext ctx) { copyFrom(ctx); }
+	}
 	public static class BareExprContext extends StatContext {
 		public ExprContext expr() {
 			return getRuleContext(ExprContext.class,0);
@@ -179,7 +201,7 @@ public class FeatherweightJavaScriptParser extends Parser {
 		StatContext _localctx = new StatContext(_ctx, getState());
 		enterRule(_localctx, 2, RULE_stat);
 		try {
-			setState(30);
+			setState(42);
 			_errHandler.sync(this);
 			switch ( getInterpreter().adaptivePredict(_input,1,_ctx) ) {
 			case 1:
@@ -228,6 +250,38 @@ public class FeatherweightJavaScriptParser extends Parser {
 				block();
 				}
 				break;
+			case 4:
+				_localctx = new WhileContext(_localctx);
+				enterOuterAlt(_localctx, 4);
+				{
+				setState(30);
+				match(WHILE);
+				setState(31);
+				match(T__0);
+				setState(32);
+				expr(0);
+				setState(33);
+				match(T__1);
+				setState(34);
+				block();
+				}
+				break;
+			case 5:
+				_localctx = new PrintContext(_localctx);
+				enterOuterAlt(_localctx, 5);
+				{
+				setState(36);
+				match(PRINT);
+				setState(37);
+				match(T__0);
+				setState(38);
+				expr(0);
+				setState(39);
+				match(T__1);
+				setState(40);
+				block();
+				}
+				break;
 			}
 		}
 		catch (RecognitionException re) {
@@ -268,9 +322,54 @@ public class FeatherweightJavaScriptParser extends Parser {
 		}
 		public MulDivModContext(ExprContext ctx) { copyFrom(ctx); }
 	}
+	public static class FunctionApplicationContext extends ExprContext {
+		public List<ExprContext> expr() {
+			return getRuleContexts(ExprContext.class);
+		}
+		public ExprContext expr(int i) {
+			return getRuleContext(ExprContext.class,i);
+		}
+		public FunctionApplicationContext(ExprContext ctx) { copyFrom(ctx); }
+	}
+	public static class AddSubContext extends ExprContext {
+		public Token op;
+		public List<ExprContext> expr() {
+			return getRuleContexts(ExprContext.class);
+		}
+		public ExprContext expr(int i) {
+			return getRuleContext(ExprContext.class,i);
+		}
+		public AddSubContext(ExprContext ctx) { copyFrom(ctx); }
+	}
+	public static class ComparisonsContext extends ExprContext {
+		public Token op;
+		public List<ExprContext> expr() {
+			return getRuleContexts(ExprContext.class);
+		}
+		public ExprContext expr(int i) {
+			return getRuleContext(ExprContext.class,i);
+		}
+		public ComparisonsContext(ExprContext ctx) { copyFrom(ctx); }
+	}
+	public static class FunctionDeclarationContext extends ExprContext {
+		public TerminalNode FUNCTION() { return getToken(FeatherweightJavaScriptParser.FUNCTION, 0); }
+		public TerminalNode IDENTIFIER() { return getToken(FeatherweightJavaScriptParser.IDENTIFIER, 0); }
+		public BlockContext block() {
+			return getRuleContext(BlockContext.class,0);
+		}
+		public FunctionDeclarationContext(ExprContext ctx) { copyFrom(ctx); }
+	}
 	public static class IntContext extends ExprContext {
 		public TerminalNode INT() { return getToken(FeatherweightJavaScriptParser.INT, 0); }
 		public IntContext(ExprContext ctx) { copyFrom(ctx); }
+	}
+	public static class VariableDeclarationContext extends ExprContext {
+		public TerminalNode VAR() { return getToken(FeatherweightJavaScriptParser.VAR, 0); }
+		public TerminalNode IDENTIFIER() { return getToken(FeatherweightJavaScriptParser.IDENTIFIER, 0); }
+		public ExprContext expr() {
+			return getRuleContext(ExprContext.class,0);
+		}
+		public VariableDeclarationContext(ExprContext ctx) { copyFrom(ctx); }
 	}
 
 	public final ExprContext expr() throws RecognitionException {
@@ -289,7 +388,7 @@ public class FeatherweightJavaScriptParser extends Parser {
 			int _alt;
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(38);
+			setState(69);
 			_errHandler.sync(this);
 			switch (_input.LA(1)) {
 			case INT:
@@ -298,7 +397,7 @@ public class FeatherweightJavaScriptParser extends Parser {
 				_ctx = _localctx;
 				_prevctx = _localctx;
 
-				setState(33);
+				setState(45);
 				match(INT);
 				}
 				break;
@@ -307,50 +406,153 @@ public class FeatherweightJavaScriptParser extends Parser {
 				_localctx = new ParensContext(_localctx);
 				_ctx = _localctx;
 				_prevctx = _localctx;
-				setState(34);
+				setState(46);
 				match(T__0);
-				setState(35);
+				setState(47);
 				expr(0);
-				setState(36);
+				setState(48);
 				match(T__1);
+				}
+				break;
+			case FUNCTION:
+				{
+				_localctx = new FunctionDeclarationContext(_localctx);
+				_ctx = _localctx;
+				_prevctx = _localctx;
+				setState(50);
+				match(FUNCTION);
+				setState(51);
+				match(T__0);
+				setState(52);
+				match(IDENTIFIER);
+				setState(53);
+				match(T__1);
+				setState(54);
+				match(T__2);
+				setState(55);
+				block();
+				setState(56);
+				match(T__3);
+				}
+				break;
+			case T__4:
+				{
+				_localctx = new FunctionApplicationContext(_localctx);
+				_ctx = _localctx;
+				_prevctx = _localctx;
+				setState(58);
+				match(T__4);
+				setState(59);
+				match(T__0);
+				setState(60);
+				expr(0);
+				setState(61);
+				match(T__5);
+				setState(62);
+				expr(0);
+				setState(63);
+				match(T__1);
+				}
+				break;
+			case VAR:
+				{
+				_localctx = new VariableDeclarationContext(_localctx);
+				_ctx = _localctx;
+				_prevctx = _localctx;
+				setState(65);
+				match(VAR);
+				setState(66);
+				match(IDENTIFIER);
+				setState(67);
+				match(T__6);
+				setState(68);
+				expr(1);
 				}
 				break;
 			default:
 				throw new NoViableAltException(this);
 			}
 			_ctx.stop = _input.LT(-1);
-			setState(45);
+			setState(82);
 			_errHandler.sync(this);
-			_alt = getInterpreter().adaptivePredict(_input,3,_ctx);
+			_alt = getInterpreter().adaptivePredict(_input,4,_ctx);
 			while ( _alt!=2 && _alt!=org.antlr.v4.runtime.atn.ATN.INVALID_ALT_NUMBER ) {
 				if ( _alt==1 ) {
 					if ( _parseListeners!=null ) triggerExitRuleEvent();
 					_prevctx = _localctx;
 					{
-					{
-					_localctx = new MulDivModContext(new ExprContext(_parentctx, _parentState));
-					pushNewRecursionContext(_localctx, _startState, RULE_expr);
-					setState(40);
-					if (!(precpred(_ctx, 3))) throw new FailedPredicateException(this, "precpred(_ctx, 3)");
-					setState(41);
-					((MulDivModContext)_localctx).op = _input.LT(1);
-					_la = _input.LA(1);
-					if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << T__2) | (1L << MUL) | (1L << DIV))) != 0)) ) {
-						((MulDivModContext)_localctx).op = (Token)_errHandler.recoverInline(this);
-					}
-					else {
-						if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
-						_errHandler.reportMatch(this);
-						consume();
-					}
-					setState(42);
-					expr(4);
+					setState(80);
+					_errHandler.sync(this);
+					switch ( getInterpreter().adaptivePredict(_input,3,_ctx) ) {
+					case 1:
+						{
+						_localctx = new MulDivModContext(new ExprContext(_parentctx, _parentState));
+						pushNewRecursionContext(_localctx, _startState, RULE_expr);
+						setState(71);
+						if (!(precpred(_ctx, 8))) throw new FailedPredicateException(this, "precpred(_ctx, 8)");
+						setState(72);
+						((MulDivModContext)_localctx).op = _input.LT(1);
+						_la = _input.LA(1);
+						if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << MUL) | (1L << DIV) | (1L << MOD))) != 0)) ) {
+							((MulDivModContext)_localctx).op = (Token)_errHandler.recoverInline(this);
+						}
+						else {
+							if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
+							_errHandler.reportMatch(this);
+							consume();
+						}
+						setState(73);
+						expr(9);
+						}
+						break;
+					case 2:
+						{
+						_localctx = new AddSubContext(new ExprContext(_parentctx, _parentState));
+						pushNewRecursionContext(_localctx, _startState, RULE_expr);
+						setState(74);
+						if (!(precpred(_ctx, 7))) throw new FailedPredicateException(this, "precpred(_ctx, 7)");
+						setState(75);
+						((AddSubContext)_localctx).op = _input.LT(1);
+						_la = _input.LA(1);
+						if ( !(_la==PLUS || _la==MIN) ) {
+							((AddSubContext)_localctx).op = (Token)_errHandler.recoverInline(this);
+						}
+						else {
+							if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
+							_errHandler.reportMatch(this);
+							consume();
+						}
+						setState(76);
+						expr(8);
+						}
+						break;
+					case 3:
+						{
+						_localctx = new ComparisonsContext(new ExprContext(_parentctx, _parentState));
+						pushNewRecursionContext(_localctx, _startState, RULE_expr);
+						setState(77);
+						if (!(precpred(_ctx, 6))) throw new FailedPredicateException(this, "precpred(_ctx, 6)");
+						setState(78);
+						((ComparisonsContext)_localctx).op = _input.LT(1);
+						_la = _input.LA(1);
+						if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << GT) | (1L << LT) | (1L << GE) | (1L << LE) | (1L << EQ))) != 0)) ) {
+							((ComparisonsContext)_localctx).op = (Token)_errHandler.recoverInline(this);
+						}
+						else {
+							if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
+							_errHandler.reportMatch(this);
+							consume();
+						}
+						setState(79);
+						expr(7);
+						}
+						break;
 					}
 					} 
 				}
-				setState(47);
+				setState(84);
 				_errHandler.sync(this);
-				_alt = getInterpreter().adaptivePredict(_input,3,_ctx);
+				_alt = getInterpreter().adaptivePredict(_input,4,_ctx);
 			}
 			}
 		}
@@ -397,40 +599,45 @@ public class FeatherweightJavaScriptParser extends Parser {
 		enterRule(_localctx, 6, RULE_block);
 		int _la;
 		try {
-			setState(57);
+			setState(94);
 			_errHandler.sync(this);
 			switch (_input.LA(1)) {
-			case T__3:
+			case T__2:
 				_localctx = new FullBlockContext(_localctx);
 				enterOuterAlt(_localctx, 1);
 				{
-				setState(48);
-				match(T__3);
-				setState(52);
+				setState(85);
+				match(T__2);
+				setState(89);
 				_errHandler.sync(this);
 				_la = _input.LA(1);
-				while ((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << T__0) | (1L << IF) | (1L << INT))) != 0)) {
+				while ((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << T__0) | (1L << T__4) | (1L << IF) | (1L << WHILE) | (1L << FUNCTION) | (1L << VAR) | (1L << PRINT) | (1L << INT))) != 0)) {
 					{
 					{
-					setState(49);
+					setState(86);
 					stat();
 					}
 					}
-					setState(54);
+					setState(91);
 					_errHandler.sync(this);
 					_la = _input.LA(1);
 				}
-				setState(55);
-				match(T__4);
+				setState(92);
+				match(T__3);
 				}
 				break;
 			case T__0:
+			case T__4:
 			case IF:
+			case WHILE:
+			case FUNCTION:
+			case VAR:
+			case PRINT:
 			case INT:
 				_localctx = new SimpBlockContext(_localctx);
 				enterOuterAlt(_localctx, 2);
 				{
-				setState(56);
+				setState(93);
 				stat();
 				}
 				break;
@@ -459,29 +666,41 @@ public class FeatherweightJavaScriptParser extends Parser {
 	private boolean expr_sempred(ExprContext _localctx, int predIndex) {
 		switch (predIndex) {
 		case 0:
-			return precpred(_ctx, 3);
+			return precpred(_ctx, 8);
+		case 1:
+			return precpred(_ctx, 7);
+		case 2:
+			return precpred(_ctx, 6);
 		}
 		return true;
 	}
 
 	public static final String _serializedATN =
-		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\26>\4\2\t\2\4\3\t"+
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\"c\4\2\t\2\4\3\t"+
 		"\3\4\4\t\4\4\5\t\5\3\2\6\2\f\n\2\r\2\16\2\r\3\3\3\3\3\3\3\3\3\3\3\3\3"+
-		"\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\5\3!\n\3\3\4\3\4\3\4\3\4\3"+
-		"\4\3\4\5\4)\n\4\3\4\3\4\3\4\7\4.\n\4\f\4\16\4\61\13\4\3\5\3\5\7\5\65\n"+
-		"\5\f\5\16\58\13\5\3\5\3\5\5\5<\n\5\3\5\2\3\6\6\2\4\6\b\2\3\4\2\5\5\21"+
-		"\22\2@\2\13\3\2\2\2\4 \3\2\2\2\6(\3\2\2\2\b;\3\2\2\2\n\f\5\4\3\2\13\n"+
-		"\3\2\2\2\f\r\3\2\2\2\r\13\3\2\2\2\r\16\3\2\2\2\16\3\3\2\2\2\17\20\5\6"+
-		"\4\2\20\21\7\23\2\2\21!\3\2\2\2\22\23\7\b\2\2\23\24\7\3\2\2\24\25\5\6"+
-		"\4\2\25\26\7\4\2\2\26\27\5\b\5\2\27\30\7\t\2\2\30\31\5\b\5\2\31!\3\2\2"+
-		"\2\32\33\7\b\2\2\33\34\7\3\2\2\34\35\5\6\4\2\35\36\7\4\2\2\36\37\5\b\5"+
-		"\2\37!\3\2\2\2 \17\3\2\2\2 \22\3\2\2\2 \32\3\2\2\2!\5\3\2\2\2\"#\b\4\1"+
-		"\2#)\7\16\2\2$%\7\3\2\2%&\5\6\4\2&\'\7\4\2\2\')\3\2\2\2(\"\3\2\2\2($\3"+
-		"\2\2\2)/\3\2\2\2*+\f\5\2\2+,\t\2\2\2,.\5\6\4\6-*\3\2\2\2.\61\3\2\2\2/"+
-		"-\3\2\2\2/\60\3\2\2\2\60\7\3\2\2\2\61/\3\2\2\2\62\66\7\6\2\2\63\65\5\4"+
-		"\3\2\64\63\3\2\2\2\658\3\2\2\2\66\64\3\2\2\2\66\67\3\2\2\2\679\3\2\2\2"+
-		"8\66\3\2\2\29<\7\7\2\2:<\5\4\3\2;\62\3\2\2\2;:\3\2\2\2<\t\3\2\2\2\b\r"+
-		" (/\66;";
+		"\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3"+
+		"\3\3\3\3\3\3\3\3\3\3\5\3-\n\3\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4"+
+		"\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\5\4H\n\4"+
+		"\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\7\4S\n\4\f\4\16\4V\13\4\3\5\3\5\7"+
+		"\5Z\n\5\f\5\16\5]\13\5\3\5\3\5\5\5a\n\5\3\5\2\3\6\6\2\4\6\b\2\5\4\2\23"+
+		"\24\27\27\3\2\25\26\3\2\31\35\2l\2\13\3\2\2\2\4,\3\2\2\2\6G\3\2\2\2\b"+
+		"`\3\2\2\2\n\f\5\4\3\2\13\n\3\2\2\2\f\r\3\2\2\2\r\13\3\2\2\2\r\16\3\2\2"+
+		"\2\16\3\3\2\2\2\17\20\5\6\4\2\20\21\7\30\2\2\21-\3\2\2\2\22\23\7\n\2\2"+
+		"\23\24\7\3\2\2\24\25\5\6\4\2\25\26\7\4\2\2\26\27\5\b\5\2\27\30\7\13\2"+
+		"\2\30\31\5\b\5\2\31-\3\2\2\2\32\33\7\n\2\2\33\34\7\3\2\2\34\35\5\6\4\2"+
+		"\35\36\7\4\2\2\36\37\5\b\5\2\37-\3\2\2\2 !\7\f\2\2!\"\7\3\2\2\"#\5\6\4"+
+		"\2#$\7\4\2\2$%\5\b\5\2%-\3\2\2\2&\'\7\17\2\2\'(\7\3\2\2()\5\6\4\2)*\7"+
+		"\4\2\2*+\5\b\5\2+-\3\2\2\2,\17\3\2\2\2,\22\3\2\2\2,\32\3\2\2\2, \3\2\2"+
+		"\2,&\3\2\2\2-\5\3\2\2\2./\b\4\1\2/H\7\20\2\2\60\61\7\3\2\2\61\62\5\6\4"+
+		"\2\62\63\7\4\2\2\63H\3\2\2\2\64\65\7\r\2\2\65\66\7\3\2\2\66\67\7\36\2"+
+		"\2\678\7\4\2\289\7\5\2\29:\5\b\5\2:;\7\6\2\2;H\3\2\2\2<=\7\7\2\2=>\7\3"+
+		"\2\2>?\5\6\4\2?@\7\b\2\2@A\5\6\4\2AB\7\4\2\2BH\3\2\2\2CD\7\16\2\2DE\7"+
+		"\36\2\2EF\7\t\2\2FH\5\6\4\3G.\3\2\2\2G\60\3\2\2\2G\64\3\2\2\2G<\3\2\2"+
+		"\2GC\3\2\2\2HT\3\2\2\2IJ\f\n\2\2JK\t\2\2\2KS\5\6\4\13LM\f\t\2\2MN\t\3"+
+		"\2\2NS\5\6\4\nOP\f\b\2\2PQ\t\4\2\2QS\5\6\4\tRI\3\2\2\2RL\3\2\2\2RO\3\2"+
+		"\2\2SV\3\2\2\2TR\3\2\2\2TU\3\2\2\2U\7\3\2\2\2VT\3\2\2\2W[\7\5\2\2XZ\5"+
+		"\4\3\2YX\3\2\2\2Z]\3\2\2\2[Y\3\2\2\2[\\\3\2\2\2\\^\3\2\2\2][\3\2\2\2^"+
+		"a\7\6\2\2_a\5\4\3\2`W\3\2\2\2`_\3\2\2\2a\t\3\2\2\2\t\r,GRT[`";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
--- proj_master/FeatherweightJavaScript.g4
+++ proj_master/FeatherweightJavaScript.g4
@@ -15,14 +15,24 @@ INT: [1-9][0-9]* | '0';
 BOOL: 'true' | 'false';
 NULL: 'null';
 
-// Symbols
+// Symbols 
 MUL: '*';
 DIV: '/';
+PLUS: '+';
+MIN: '-';
+MOD: '%';
 SEPARATOR: ';';
+GT: '>';
+LT: '<';
+GE: '>=';
+LE: '<=';
+EQ: '==';
+IDENTIFIER: [a-zA-Z_][a-zA-Z_0-9]*;
 
 // Whitespace and comments
 NEWLINE: '\r'? '\n' -> skip;
 LINE_COMMENT: '//' ~[\n\r]* -> skip;
+BLOCK_COMMENT: '/*' .*? '*/' -> skip;
 WS: [ \t]+ -> skip; // ignore whitespace
 
 // ***Parsing rules ***
@@ -33,12 +43,23 @@ prog: stat+;
 stat:
 	expr SEPARATOR						# bareExpr
 	| IF '(' expr ')' block ELSE block	# ifThenElse
-	| IF '(' expr ')' block				# ifThen;
+	| IF '(' expr ')' block				# ifThen
+	| WHILE '(' expr ')' block			# while
+	| PRINT '(' expr ')' block			# print;
 
 expr:
-	expr op = ('*' | '/' | '%') expr	# MulDivMod
-	| INT								# int
-	| '(' expr ')'						# parens;
+	expr op = ('*' | '/' | '%') expr					# MulDivMod
+	| expr op = ('+' | '-') expr						# AddSub
+	| expr op = ('<' | '<=' | '>' | '>=' | '==') expr	# Comparisons
+	| '(' expr ')'										# parens
+	| FUNCTION '(' IDENTIFIER ')' '{' block '}'			# functionDeclaration
+	| 'f' '(' expr ',' expr ')'							# functionApplication
+	| VAR IDENTIFIER '=' expr							# variableDeclaration
+	| expr												# variableReference
+	| IDENTIFIER '=' expr								# assignmentStatement
+	| INT												# int
+	| BOOL												# boolean
+	| NULL												# null;
 
 block: '{' stat* '}' # fullBlock | stat # simpBlock;
 
